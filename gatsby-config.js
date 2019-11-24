cdb849240b4b43068e0efc3cd3d3611c7013cc5b
John Walley <john@Johns-MBP.lan>
2019-11-24 18:33:45 +0000

    Update dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,12 +18,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `svg-rowing-blades`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `react-rowing-blades`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
