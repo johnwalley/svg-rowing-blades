a967b702db905db34f05a87fc69172fb9aa87e0e
johnwalley <john@walley.org.uk>
2021-08-13 09:50:16 +0100

    fix: images

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,16 +1,59 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
+import { Disclosure } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/outline";
 
+const faqs = [
+  {
+    question: "I can't find the blade I'm looking for?",
+    answer: "Let us know and we might be able to add it as a new design.",
+  },
+  // More questions...
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
 const AboutPage = () => (
-  <Layout title="About | Next.js + TypeScript Example">
-    <h1>About</h1>
-    <p>This is the about page</p>
-    <p>
-      <Link href="/">
-        <a>Go home</a>
-      </Link>
-    </p>
+  <Layout title="SVG ROwing Blades | About">
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:py-16 sm:px-6 lg:px-8">
+        <div className="max-w-3xl mx-auto divide-y-2 divide-gray-200">
+          <h2 className="text-center text-3xl font-extrabold text-gray-900 sm:text-4xl">
+            Frequently asked questions
+          </h2>
+          <dl className="mt-6 space-y-6 divide-y divide-gray-200">
+            {faqs.map((faq) => (
+              <Disclosure as="div" key={faq.question} className="pt-6">
+                {({ open }) => (
+                  <>
+                    <dt className="text-lg">
+                      <Disclosure.Button className="text-left w-full flex justify-between items-start text-gray-400">
+                        <span className="font-medium text-gray-900">
+                          {faq.question}
+                        </span>
+                        <span className="ml-6 h-7 flex items-center">
+                          <ChevronDownIcon
+                            className={classNames(
+                              open ? "-rotate-180" : "rotate-0",
+                              "h-6 w-6 transform"
+                            )}
+                            aria-hidden="true"
+                          />
+                        </span>
+                      </Disclosure.Button>
+                    </dt>
+                    <Disclosure.Panel as="dd" className="mt-2 pr-12">
+                      <p className="text-base text-gray-500">{faq.answer}</p>
+                    </Disclosure.Panel>
+                  </>
+                )}
+              </Disclosure>
+            ))}
+          </dl>
+        </div>
+      </div>
+    </div>
   </Layout>
-)
+);
 
-export default AboutPage
+export default AboutPage;
--- pages/club/[club].tsx
+++ pages/club/[club].tsx
@@ -17,25 +17,26 @@ const Club = () => {
 
   return (
     <Layout title={`SVG Rowing Blades | ${allNames[club as string]}`}>
-      <div className="sm:flex">
-        <div className="mb-4 flex-shrink-0 sm:mb-0 sm:mr-4">
-          <Blade
-            club={club as string}
-            size={100}
-            className="h-16 w-16 border border-gray-300 bg-white text-gray-300"
-          />
+      <div className="flex flex-col items-center">
+        <div className="sm:flex">
+          <div className="mb-4 flex-shrink-0 sm:mb-0 sm:mr-4">
+            <Blade
+              club={club as string}
+              size={100}
+              className="h-32 w-32 border border-gray-300 bg-white text-gray-300"
+            />
+          </div>
+          <div>
+            <h4 className="text-lg font-bold">{allNames[club as string]}</h4>
+            <p className="mt-1">{club}</p>
+          </div>
         </div>
-        <div>
-          <h4 className="text-lg font-bold">{allNames[club as string]}</h4>
-          <p className="mt-1">{club}</p>
-        </div>
+        <a href={`/images/${club}.svg`} download={`${club}.svg`}>
+          <button className="mt-2 bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded">
+            Download
+          </button>
+        </a>
       </div>
-      {/* <Blade club="emm" size={100} /> */}
-      <a href={`/${"emm"}.svg`} download={`${"emm"}.svg`}>
-        <button className="bg-pink-500 hover:bg-pink-700 text-white font-bold py-2 px-4 rounded">
-          Download
-        </button>
-      </a>
     </Layout>
   );
 };
