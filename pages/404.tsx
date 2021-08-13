b5fc711af001dc6d64569ce8d22ffdb7c27b422e
johnwalley <john@walley.org.uk>
2021-08-13 18:32:06 +0100

    fix: escape '

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
     <main
@@ -15,15 +17,14 @@ export default function NotFound() {
           Uh oh! I think you’re lost.
         </h1>
         <p className="mt-2 text-lg font-medium text-black text-opacity-50">
-          It looks like the page you’re looking for doesn't exist.
+          It looks like the page you&apos;re looking for doesn&apos;t exist.
         </p>
         <div className="mt-6">
-          <a
-            href="/"
-            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-black text-opacity-75 bg-white bg-opacity-75 sm:bg-opacity-25 sm:hover:bg-opacity-50"
-          >
-            Go back home
-          </a>
+          <Link href="/">
+            <a className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-black text-opacity-75 bg-white bg-opacity-75 sm:bg-opacity-25 sm:hover:bg-opacity-50">
+              Go back home
+            </a>
+          </Link>
         </div>
       </div>
     </main>
